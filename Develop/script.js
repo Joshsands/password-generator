8382937c859cadf79a695624f65818ab6c45b8a5
JoshSands <jsands@post.com>
2021-05-24 07:59:27 -0500

    changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,5 @@
-//// create arrays for special character types
-
-var caseArray= [];
-var lowerCaseChar=[
-  "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
-];
-var upperCaseChar=[
-  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
-var numericChar=[
-  "1","2","3","4","5","6","7","8","9","0"
-];
-var specialChar=[
-  "!","@","#","$","%","^","&","*","(",")","[","]","|","{","}",":",";","'","<",">","?","/"
-];
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-var selectLength = function () {
-  var passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."))
+function selectLength () {
+  passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."))
   if(passwordLength > 8 && passwordLength < 128) {
     console.log(passwordLength)
     return passwordLength;
@@ -26,81 +9,86 @@ var selectLength = function () {
       }
 };
 
-var selectCriteria = function () {
-      var inputLowerCase=confirm("Do you want to include lowercase characters?");
-      var inputUpperCase=confirm("Do you want to include uppercase characters?");
-      var inputNumeric=confirm("Do you want to include numeric characters?");
-      var inputSpecial=confirm("Do you want to include special characters?");
-        
-      //THEN my input should be validated and at least one character type should be selected
-    if (inputLowerCase == true) {
-      return inputLowerCase
-    }if (inputUpperCase == true) {
-      return inputUpperCase
-    }if (inputNumeric == true) {
-      return inputNumeric
-    }if (inputSpecial == true) {
-    return inputSpecial 
-    
-  } else {
-      alert("Please select one type of character");
-      return selectCriteria();
-    }
+//function selectCriteria () {
+
+
+//} else {
+  //alert("Please select one type of character");
+  //return selectCriteria();
+//};
+//};
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
 };
 
-var passwordInfo = {
-  length: selectLength(),
-  lowercase: selectCriteria(),
-};
+generate.addEventListener('click', () => {
+const passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."))
 
-// create a function for generating the password
-function generatePassword() {
-  selectLength();  
-  selectCriteria();
+const inputLowerCase=confirm("Do you want to include lowercase characters?");
+const inputUpperCase=confirm("Do you want to include uppercase characters?");
+const inputNumeric=confirm("Do you want to include numeric characters?");
+const inputSpecial=confirm("Do you want to include special characters?");
 
-  var passwordOutput="";
-    
-      // WHEN all prompts are answered
-      // THEN a password is generated that matches the selected criteria
-    
-      if (inputLowerCase==true){
-          caseArray.push(lowerCaseChar);
-      
-      }
-      if (inputUpperCase==true){
-          caseArray.push(upperCaseChar)
-      
-      }
-      if (inputNumeric==true){
-          caseArray.push(numericChar)
-          
-      }
-      if (inputSpecial==true){
-          caseArray.push(specialChar)
-      }
-    
-      //  inputs charcters based on password prompt length
-      for(var i=0; i<passwordLength; i++){
-    
-          var randomCharArrayNum;
-          var selectedCharArray;
-          var randomCharNum;
-          var randomChar;
-          
-          randomCharArrayNum= parseInt(Math.floor(Math.random()*caseArray.length));
-          selectedCharArray=caseArray[randomCharArrayNum];
-          randomCharNum=Math.floor(Math.random()*selectedCharArray.length);
-          randomChar=selectedCharArray[randomCharNum];
-          passwordOutput+=randomChar;      
-      }
-      passwordEntry.textContent=passwordOutput;    
-    
-  }
+const hasLength = passwordLength;
+const hasLower = inputLowerCase;
+const hasUpper = inputUpperCase;
+const hasNumber = inputNumeric;
+const hasSymbol = inputSpecial;
 
-  //WHEN I answer each prompt
+password.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, hasLength);
+});
 
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+
+  const typesCount = lower + upper + number + symbol;
+  console.log(typesCount)
+
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(
+    item => Object.values(item)[0]
+  );
+console.log('typesArr: ', typesArr);
+if (typesCount === 0) {
+  return '';
+}
+for(let i = 0; i < length; i += typesCount) {
+  typesArr.forEach(type => {
+const funcName = Object.keys(type)[0];
+console.log('funcName: ', funcName);
+
+generatedPassword += randomFunc[funcName]();
+  });
+}
+const finalPassword = generatedPassword;
+return finalPassword;
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()[]{}=+<>?/\.,';
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+
+
+
+
 var generateButton=document.getElementById("generate");
 var passwordEntry=document.getElementById("password");
-generateButton.onclick = generatePassword;
